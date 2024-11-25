abc9c77dc18494253e8924b82f224ea6b7c4a169
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-25 23:10:51 +0530

    Add schema with yup for validation

---
+++ src/validation/schema.ts
@@ -0,0 +1,34 @@
+import * as yup from 'yup';
+
+export const loginSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .matches(/^\S*$/, 'No spaces allowed')
+    .required('Username is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(12, 'Password must not exceed 12 characters')
+    .matches(/^\S*$/, 'No spaces allowed')
+    .required('Password is required'),
+});
+
+export const userSchema = yup.object().shape({
+  firstName: yup.string().required('First name is required'),
+  middleName: yup.string(),
+  lastName: yup.string().required('Last name is required'),
+  username: yup
+    .string()
+    .max(20, 'Username must not exceed 20 characters')
+    .required('Username is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(16, 'Password must not exceed 16 characters')
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required('Confirm password is required'),
+});
