b451e73e8027bd3311265919e0a99312f4fec69a
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-25 23:11:19 +0530

    Add protected route based on token

---
+++ src/components/ProtectedRoutes.tsx
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { token } = useSelector((state: RootState) => state.auth);
+
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
