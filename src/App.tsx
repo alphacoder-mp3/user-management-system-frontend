3765e02fab315b256c320d0eb1f6b7925b4a969b
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-27 21:29:43 +0530

    Add redux persist implementation to persist the state even after refresh

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,33 @@ import Login from './pages/login';
 import { Suspense, lazy } from 'react';
 const Dashboard = lazy(() => import('./components/dashboard'));
 import ProtectedRoute from './components/protected-routes';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Suspense fallback={<div>Loading...</div>}>
-                    {' '}
-                    <Dashboard />
-                  </Suspense>
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-        <Toaster position="top-right" />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Suspense fallback={<div>Loading...</div>}>
+                      {' '}
+                      <Dashboard />
+                    </Suspense>
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+          <Toaster position="top-right" />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/auth-slice';
 import userReducer from './slices/user-slice';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    users: userReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['auth'], // only auth will be persisted
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  users: userReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
