1b59aba31c112194cc0c93f4ee237d9655c60346
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-27 18:59:57 +0530

    Update the schema on front end side

--- src/validation/schema.ts
+++ src/validation/schema.ts
@@ -5,11 +5,11 @@ export const loginSchema = yup.object().shape({
     .string()
     .min(3, 'Email must be at least 3 characters')
     .matches(/^\S*$/, 'No spaces allowed')
-    .required('Username is required'),
+    .required('Email is required'),
   password: yup
     .string()
     .min(8, 'Password must be at least 8 characters')
-    .max(12, 'Password must not exceed 12 characters')
+    .max(20, 'Password must not exceed 20 characters')
     .matches(/^\S*$/, 'No spaces allowed')
     .required('Password is required'),
 });
@@ -21,7 +21,7 @@ export const userSchema = yup.object().shape({
   email: yup
     .string()
     .max(20, 'Email must not exceed 20 characters')
-    .required('Username is required'),
+    .required('Email is required'),
   password: yup
     .string()
     .min(8, 'Password must be at least 8 characters')
